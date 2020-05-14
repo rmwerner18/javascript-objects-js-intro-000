14ca41a51239879c193c3eea7cc114fcdeefb8dd
Ryan Werner <rmwerner@umich.edu>
2020-05-14 02:07:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,2 +1,3 @@
 var playlist = {the Beatles: "Hey Jude"}
-var updatePlaylist = (playlist, artistName, songTitle) => {return Object.assign({}, playlist, {[artistName]: songTitle })}
+var updatePlaylist = (playlist, artistName, songTitle) => {playlist[artistName] = songTitle}
+
